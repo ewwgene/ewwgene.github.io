d18909c4955e97cad4c54de7316eeb1b93fa9619
r0cam <33371607+ewwgene@users.noreply.github.com>
2023-02-25 17:41:47 +0700

    sum

--- CreateIndex.py
+++ CreateIndex.py
@@ -302,6 +302,7 @@ _%s-%s._
 
 %s
 
+
 ''' % (
                 project, urlProject, info['date'][0], info['date'][1], info['overview'][0:99], urlProject, infoMedium(info['medium']), imgMain2(projectFolder, urlProject, project, info['medium'][0]))
                 ################################################################################################################
