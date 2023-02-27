ce49c54b6fd5b0959af6096794e2a1e3d364deea
r0cam <33371607+ewwgene@users.noreply.github.com>
2023-02-27 08:20:41 +0700

    sum

--- CreateIndex.py
+++ CreateIndex.py
@@ -12,9 +12,9 @@ allImage=[]
 allImageText=''
 
 header= '## [_DESIGN_/](https://ewwgene.github.io/DESIGN)<br>[_ART_/](https://ewwgene.github.io/ART)<br>[_PROGRAMMING_/](https://ewwgene.github.io/PROGRAMMING)\n'
-headerDESIGN= '# [' + Name + '](' + urlHome + ') / _DESIGN_ '
-headerART= '# [' + Name + '](' + urlHome + ') / _ART_ '
-headerPROGRAMMING= '# [' + Name + '](' + urlHome + ') / _PROGRAMMING_ '
+headerDESIGN= '# [' + Name + ' /](' + urlHome + ') _DESIGN_ '
+headerART= '# [' + Name + ' /](' + urlHome + ') / _ART_ '
+headerPROGRAMMING= '# [' + Name + ' /](' + urlHome + ') _PROGRAMMING_ '
 
 footer= about + mailTo
 
